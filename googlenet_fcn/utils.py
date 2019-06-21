bdc8273f4489199a20b99dd97d0e4b249b3704f2
Michael Kösel <michaelkoesel@gmx.de>
2019-06-21 22:09:11 +0200

    Fix recursion

--- googlenet_fcn/utils.py
+++ googlenet_fcn/utils.py
@@ -52,11 +52,12 @@ def collate_fn(batch):
 def freeze_batchnorm(module):
     def train(self, mode=True):
         self.training = mode
+        for m in self.children():
+            m.train(mode)
+
         for m in self.modules():
             if isinstance(m, nn.BatchNorm2d):
                 m.eval()
-            else:
-                m.train(mode)
         return self
 
     module.train = types.MethodType(train, module)
